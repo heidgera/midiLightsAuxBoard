ef37db832746974362be3a304e15a148b2b4b69c
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-08-14 16:20:37 -0500

    trying to write leds from electron

--- local/src/app.js
+++ local/src/app.js
@@ -12,16 +12,18 @@ obtain(['µ/midi.js', './src/LEDs.js'], (midi, { LEDs })=> {//, './src/LEDs.js'
       LEDs.show();
     }, 1000);
 
+    console.log(midi.in.getMIDIInDevices());
+
     midi.in.setNoteHandler((note, vel)=> {
       if (note > 0 && note < 88) {
-        //console.log('Note ' + note + ' pressed at ' + vel);
+        console.log('Note ' + note + ' pressed at ' + vel);
         /*var c = note % 12;
         var k = (note % 4) * vel / 4;
         if (c > 8) r = 0, g = 1, b = 0;
         else if (c > 4) r = 0, g = 0, b = 1;
         else r = 1, g = 0, b = 0;
         LEDs.setColor(note, r * (vel - k) + b * k, g * (vel - k) + r * k, b * (vel - k) + g * k);*/
-        LEDs.setColor(note, vel,vel, vel);
+        LEDs.setColor(note, vel, vel, vel);
         LEDs.show();
       }
     });
