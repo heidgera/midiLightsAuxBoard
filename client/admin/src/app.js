39cd4f9f1a0e7daf7fc9f6ae4e12545efb99490c
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-09-29 14:52:08 -0500

    changed colors for keys

---
+++ client/admin/src/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+obtain(['µ/commandClient.js'], ({ MuseControl })=> {
+  exports.app = {};
+
+  var control = new MuseControl(window.location.hostname);
+
+  exports.app.start = ()=> {
+
+    control.connect();
+
+    var lights = [];
+
+    for (var i = 0; i < 88; i++) {
+      lights[i] = [0, 0, 0];
+    }
+
+    lights[44] = [255, 0, 0];
+
+    document.onkeypress = (e)=> {
+      if (e.key == ' ') console.log('Space pressed');
+      else if (e.key == 'k') {
+        control.send({ setLights: lights });
+        console.log('Sent');
+      }
+    };
+
+  };
+
+  provide(exports);
+});
