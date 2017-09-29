0be54cc7b7380d1768fcbd94ccfcf5a27e079650
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-09-29 13:25:58 -0500

    adding client

--- local/src/server/express.js
+++ local/src/server/express.js
@@ -1,8 +1,8 @@
-obtain(['express', 'fs'], (express, fs)=> {
+obtain(['express', 'fs', 'path'], (express, fs, path)=> {
   var fileServer = express();
 
-  fileServer.use('', express.static('../client'));
-  fileServer.use('/common', express.static('../common'));
+  fileServer.use('', express.static(path.join(__dirname, '../client')));
+  fileServer.use('/common', express.static(path.join(__dirname, '../common')));
 
   fileServer.listen(80, function () {
     console.log('listening on 80');
