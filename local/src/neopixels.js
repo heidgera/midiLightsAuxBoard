3944464910446f74d5dfaf6267fa5350902f9c46
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-10-03 15:39:33 -0500

    adding chords

--- local/src/neopixels.js
+++ local/src/neopixels.js
@@ -1,4 +1,4 @@
-obtain(['µ/color.js'], ({ rainbow, Color }, ws2812)=> {//'rpi-ws281x-native'
+obtain(['µ/color.js', 'rpi-ws281x-native'], ({ rainbow, Color }, ws2812)=> {//'rpi-ws281x-native'
 
   exports.Color = Color;
 
@@ -8,13 +8,13 @@ obtain(['µ/color.js'], ({ rainbow, Color }, ws2812)=> {//'rpi-ws281x-native'
     init: function (numLEDs) {
       var _this = this;
       _this.data = new Uint32Array(numLEDs);
-      //ws2812.init(numLEDs);
+      ws2812.init(numLEDs);
     },
 
     color: (r, g, b)=>((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff),
 
     show: function () {
-      //ws2812.render(this.data);
+      ws2812.render(this.data);
     },
 
     set: function (num, r, g, b) {
@@ -37,6 +37,6 @@ obtain(['µ/color.js'], ({ rainbow, Color }, ws2812)=> {//'rpi-ws281x-native'
       ));
     },
 
-    //reset: ws2812.reset,
+    reset: ws2812.reset,
   };
 });
